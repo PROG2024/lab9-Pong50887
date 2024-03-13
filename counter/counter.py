c2bdb82037258b58b3b67b7d34f7dd712bea2fb7
Pong <115053913+Pong50887@users.noreply.github.com>
2024-03-13 12:47:58 +0700

    Update counter.py

--- counter/counter.py
+++ counter/counter.py
@@ -9,14 +9,43 @@
       You may not have a __new__ depending on how you implement the singleton.
 """
 
+
 class Counter:
+    """
+    >>> counter = Counter()
+    >>> counter.count
+    0
+    >>> counter.count
+    0
+    >>> counter.increment()
+    >>> counter.count
+    1
+    >>> counter2 = Counter()
+    >>> counter2 is counter
+    True
+    >>> counter2.count
+    1
+    """
+
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
 
     def __init__(self):
+        if hasattr(self, "_initialized"):
+            return
+        self._initialized = True
         self.__count = 0
-
 
     def __str__(self):
         return f"{self.__count}"
 
-    #TODO write count property
-    #TODO write increment method
+    @property
+    def count(self):
+        return self.__count
+
+    def increment(self):
+        self.__count += 1
